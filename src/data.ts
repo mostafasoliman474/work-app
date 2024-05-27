8da647a839bad81f9279a4b5b282966f57567d5e
Mostafa Mohamed (C) <mWX1319573@china.huawei.com>
2024-05-27 23:10:07 +0300

    user home page

---
+++ src/data.ts
@@ -0,0 +1,189 @@
+import GroupIcon from '@mui/icons-material/Group';
+import HomeIcon from '@mui/icons-material/Home';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PublishIcon from '@mui/icons-material/Publish';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import SettingsIcon from '@mui/icons-material/Settings';
+import BackupIcon from '@mui/icons-material/Backup';
+export const NavList=[
+  {
+    id:1,
+    title:"MAIN",
+    links:[
+      {
+        id:1,
+        text:"Home Page",
+        link:"#",
+        img:HomeIcon
+      },
+      {
+        id:2,
+        text:"Profile",
+        link:"#",
+        img:GroupIcon
+      },
+    ]
+  },
+  {
+    id:2,
+    title:"Lists",
+    links:[
+      {
+        id:1,
+        text:"Users",
+        link:"#",
+        img:GroupIcon
+      },
+      {
+        id:2,
+        text:"Products",
+        link:"#",
+        img:InventoryIcon
+      },
+      {
+        id:3,
+        text:"Orders",
+        link:"#",
+        img:ListAltIcon
+
+      },
+      {
+        id:4,
+        text:"Posts",
+        link:"#",
+        img:PublishIcon
+      ,
+      },
+    ]
+  },
+  {
+    id:3,
+    title:"GENERAL",
+    links:[
+      {
+        id:1,
+        text:"Elements",
+        link:"#",
+        img:EditNoteIcon
+      },
+      {
+        id:2,
+        text:"Notes",
+        link:"#",
+        img:EditNoteIcon
+      },
+      {
+        id:3,
+        text:"Calender",
+        link:"#",
+        img:CalendarMonthIcon
+      },
+      {
+        id:4,
+        text:"Forms",
+        link:"#",
+        img:FormatAlignLeftIcon
+      },
+    ]
+  },
+  {
+    id:4,
+    title:"MAINTENANCE",
+    links:[
+      {
+        id:1,
+        text:"Settings",
+        link:"#",
+        img:SettingsIcon
+
+      },
+      {
+        id:2,
+        text:"Backups",
+        link:"#",
+        img:BackupIcon
+      },
+    ]
+  },
+  {
+    id:5,
+    title:"ANALYTICS",
+    links:[
+      {
+        id:1,
+        text:"Charts",
+        link:"#",
+        img:BarChartIcon
+
+      },
+      {
+        id:2,
+        text:"Logs",
+        link:"#",
+        img:ListAltIcon
+      },
+    ]
+  },
+]
+export const Users= [
+    {
+        id:1,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:2,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:3,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:4,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:5,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:6,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:7,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+    {
+        id:8,
+        username:"mostafa kejoo",
+        img:"https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+        ,email:"mostafakejoo@gmail.com",
+        payed:3600
+    },
+]
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
 import Nav from '../components/Nav.tsx'
 import SideBar from '../components/SideBar.tsx'
+import { Users } from '../data.ts'
 
 const Home = () => {
   return (
     <div>
       <Nav />
-      <div className='flex '>
+      <div className='flex gap-5 '>
 
         <SideBar/>
-        <div className='flex-3'>
-          content 
+        <div className='flex-[6] flex gap-5'>
+          
+          <div className='flex-[2]'>
+            <div className='flex flex-col gap-4 rounded-xl border border-gray-400 p-3'>
+              <p className='text-3xl font-bold text-white pl-3'>Top Deals</p>
+              <div className='flex flex-col gap-5 '>
+                {Users.map(user=>(
+                <div key={user.id} className='flex items-center justify-around hover:bg-cyan-400 p-1 rounded-2xl cursor-pointer'>
+                  <img src={user.img} alt="user img" className='w-[10%] rounded-full'/>
+                  <div>
+                    <p className='text-white text-sm'>{user.username}</p>
+                    <p className='text-white text-sm'>{user.email}</p>
+                  </div>
+                  <p className='text-white text-sm font-bold'>${user.payed}</p>
+                </div>  
+
+                ))}
+              </div>  
+            </div>  
+            <div>low</div>  
+          </div> 
+          <div className='flex-[3] bg-white'>
+          <div>upper</div>  
+            <div>low</div></div> 
+          <div className='flex-[2] bg-green-100'>
+          <div>upper</div>  
+            <div>low</div></div> 
         </div>
       </div>
     </div>
